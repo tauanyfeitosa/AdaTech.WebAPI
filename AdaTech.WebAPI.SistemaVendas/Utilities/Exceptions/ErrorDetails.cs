1b0080022bd0e3627b985301ecc124f86f1822be
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-02-21 16:39:37 -0300

    create: handler para captura de exceções em todo o projeto

---
+++ AdaTech.WebAPI.SistemaVendas/Utilities/Exceptions/ErrorDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace AdaTech.WebAPI.SistemaVendas.Utilities.Exceptions
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ AdaTech.WebAPI.SistemaVendas/Utilities/Middleware/ExeceptionMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using AdaTech.WebAPI.SistemaVendas.Utilities.Exceptions;
+
+namespace AdaTech.WebAPI.SistemaVendas.Utilities.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            int originalStatusCode = httpContext.Response.StatusCode;
+
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Algo de errado aconteceu: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = exception switch
+            {
+                NotFoundException _ => StatusCodes.Status404NotFound,
+                FailCreateUpdateException _ => StatusCodes.Status422UnprocessableEntity,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var message = exception.Message;
+
+            context.Response.StatusCode = statusCode;
+
+            var errorResponse = new ErrorDetails
+            {
+                StatusCode = statusCode,
+                Message = message
+            };
+
+            await context.Response.WriteAsJsonAsync(errorResponse);
+        }
+
+    }
+}
