4ad06d0317f0761b6f411cdcd91a2bd61d6d847b
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-02-21 12:40:21 -0300

    fix: criação de dto para passagem de parâmetros no consumo de api

---
+++ AdaTech.WebAPI.SistemaVendas/DTO/EnderecoDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AdaTech.WebAPI.SistemaVendas.DTO
+{
+    public class EnderecoDTO
+    {
+        [JsonPropertyName("street")]
+        [Required(ErrorMessage = "O campo Rua é obrigatório")]
+        public string? Rua { get; set; }
+
+        [JsonIgnore]
+        public int Numero { get; set; }
+
+        [JsonPropertyName("neighborhood")]
+        [Required(ErrorMessage = "O campo Bairro é obrigatório")]
+        public string? Bairro { get; set; }
+
+        [JsonPropertyName("city")]
+        [Required(ErrorMessage = "O campo Cidade é obrigatório")]
+        public string? Cidade { get; set; }
+
+        [JsonPropertyName("state")]
+        [Required(ErrorMessage = "O campo Estado é obrigatório")]
+        public string? Estado { get; set; }
+
+        [JsonPropertyName("cep")]
+        [Required(ErrorMessage = "O campo CEP é obrigatório")]
+        public string? CEP { get; set; }
+    }
+}
