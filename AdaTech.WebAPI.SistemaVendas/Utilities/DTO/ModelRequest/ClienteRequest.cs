3f6f06fca31b319205e48509c1c786dd2ffd3222
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-02-21 17:53:45 -0300

    fix: extensão para adição do complemento do endereço

--- AdaTech.WebAPI.SistemaVendas/Utilities/DTO/ModelRequest/ClienteRequest.cs
+++ AdaTech.WebAPI.SistemaVendas/Utilities/DTO/ModelRequest/ClienteRequest.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AdaTech.WebAPI.SistemaVendas.Utilities.DTO.ModelRequest
 {
@@ -21,6 +20,10 @@ namespace AdaTech.WebAPI.SistemaVendas.Utilities.DTO.ModelRequest
         [Phone(ErrorMessage = "O número de telefone não é válido.")]
         public string Telefone { get; set; }
 
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [RegularExpression("^[0-9]{11}$", ErrorMessage = "O CPF deve conter 11 dígitos numéricos.")]
+        public string CPF { get; set; }
+
         [Required(ErrorMessage = "O CEP é obrigatório.")]
         [Range(10000000, 99999999, ErrorMessage = "O CEP deve ser um número de 8 dígitos.")]
         public int CEP { get; set; }
@@ -29,8 +32,6 @@ namespace AdaTech.WebAPI.SistemaVendas.Utilities.DTO.ModelRequest
         [Range(1, int.MaxValue, ErrorMessage = "O número deve ser positivo.")]
         public int Numero { get; set; }
 
-        [Required(ErrorMessage = "O CPF é obrigatório.")]
-        [RegularExpression("^[0-9]{11}$", ErrorMessage = "O CPF deve conter 11 dígitos numéricos.")]
-        public string CPF { get; set; }
+        public string? Complemento { get; set; }
     }
 }
