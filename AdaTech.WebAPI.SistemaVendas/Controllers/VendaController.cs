8c9326fe587a1821a4611f4814059df834c935cc
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-02-22 17:52:31 -0300

    feat (VendaController): finalização dos endpoints relacionados a venda

--- AdaTech.WebAPI.SistemaVendas/Controllers/VendaController.cs
+++ AdaTech.WebAPI.SistemaVendas/Controllers/VendaController.cs
@@ -1,6 +1,8 @@
-﻿using AdaTech.WebAPI.DadosLibrary.DTO.Objects;
+﻿using AdaTech.WebAPI.DadosLibrary.DTO.Enums;
+using AdaTech.WebAPI.DadosLibrary.DTO.Objects;
 using AdaTech.WebAPI.DadosLibrary.Repository;
 using AdaTech.WebAPI.SistemaVendas.Utilities.Attributes.Swagger;
+using AdaTech.WebAPI.SistemaVendas.Utilities.DTO;
 using AdaTech.WebAPI.SistemaVendas.Utilities.DTO.ModelRequest;
 using AdaTech.WebAPI.SistemaVendas.Utilities.Exceptions;
 using AdaTech.WebAPI.SistemaVendas.Utilities.Services;
@@ -60,7 +62,7 @@ namespace AdaTech.WebAPI.SistemaVendas.Controllers
         /// <summary>
         /// Cria uma nova venda e seus itens.
         /// </summary>
-        [HttpPost]
+        [HttpPost("create")]
         public async Task<IActionResult> Post([FromBody] VendaRequest vendaRequest)
         {
             List<ItemVenda> itensVenda = new List<ItemVenda>();
@@ -124,14 +126,119 @@ namespace AdaTech.WebAPI.SistemaVendas.Controllers
             return Ok("Criado com sucesso!");
         }
 
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        /// <summary>
+        /// Atualiza uma venda.
+        /// </summary>
+        [HttpPut("update")]
+        public async Task<IActionResult> Put(int id, [FromBody] VendaDTO vendaDTO)
         {
+            var venda = await _vendaRepository.GetByIdAsync(id);
+            if (venda == null)
+            {
+                _logger.LogWarning("Venda com ID: {Id} não encontrada.", id);
+                throw new NotFoundException("Venda não encontrada. Experimente buscar por outro ID!");
+            }
+            var cliente = await _clienteService.GetCPFAsync(vendaDTO.ClienteCPF);
+            venda.ClienteId = cliente.Id;
+            venda.DataVenda = vendaDTO.DataVenda;
+            venda.StatusVenda = vendaDTO.StatusVenda;
+
+            var success = await _vendaRepository.UpdateAsync(venda);
+
+            if (!success)
+            {
+                _logger.LogError("Falha ao atualizar a venda.");
+                throw new FailCreateUpdateException("Falha ao atualizar a venda. Tente novamente!");
+            }
+
+            _logger.LogInformation("Venda atualizada com sucesso.");
+            return Ok("Atualizado com sucesso!");
         }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        /// <summary>
+        /// Deleta uma venda.
+        /// </summary>
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int id, bool hardDelete = false)
         {
+            var venda = await _vendaRepository.GetByIdAsync(id);
+            if (venda == null)
+            {
+                _logger.LogWarning("Venda com ID: {Id} não encontrada.", id);
+                throw new NotFoundException("Venda não encontrada. Experimente buscar por outro ID!");
+            }
+
+            if (hardDelete)
+            {
+                _logger.LogInformation("Realizando hard delete para a venda com ID {Id}", id);
+                await _vendaRepository.DeleteAsync(venda.Id);
+            }
+            else
+            {
+                _logger.LogInformation("Realizando soft delete para a venda com ID {Id}", id);
+                venda.Ativo = false;
+                await _vendaRepository.UpdateAsync(venda);
+            }
+
+            _logger.LogInformation("Venda deletada com sucesso.");
+            return Ok("Deletado com sucesso!");
+        }
+
+        /// <summary>
+        /// Atualiza o status de uma venda.
+        /// </summary>
+        [HttpPatch("updateStatus")]
+        public async Task<IActionResult> Patch(int id, [FromBody] StatusVenda statusVenda)
+        {
+            var venda = await _vendaRepository.GetByIdAsync(id);
+            if (venda == null)
+            {
+                _logger.LogWarning("Venda com ID: {Id} não encontrada.", id);
+                throw new NotFoundException("Venda não encontrada. Experimente buscar por outro ID!");
+            }
+
+            venda.StatusVenda = statusVenda;
+            var success = await _vendaRepository.UpdateAsync(venda);
+
+            if (!success)
+            {
+                _logger.LogError("Falha ao atualizar o status da venda.");
+                throw new FailCreateUpdateException("Falha ao atualizar o status da venda. Tente novamente!");
+            }
+
+            _logger.LogInformation("Status da venda atualizado com sucesso.");
+            return Ok("Status da venda atualizado com sucesso!");
+        }
+
+        /// <summary>
+        /// Ativa ou Inativa uma venda.
+        /// </summary>
+        [HttpPatch("activate")]
+        public async Task<IActionResult> Patch(int id, bool ativo)
+        {
+            var venda = await _vendaRepository.GetByIdAsync(id);
+            if (venda == null)
+            {
+                _logger.LogWarning("Venda com ID: {Id} não encontrada.", id);
+                throw new NotFoundException("Venda não encontrada. Experimente buscar por outro ID!");
+            }
+            if (venda.Ativo == ativo)
+            {
+                _logger.LogWarning("Venda com ID: {Id} já está com o status {Ativo}.", id, ativo);
+                throw new ErrorInputUserException($"Venda com ID: {id} já está com o status {ativo}.");
+            }
+
+            venda.Ativo = ativo;
+            var success = await _vendaRepository.UpdateAsync(venda);
+
+            if (!success)
+            {
+                _logger.LogError("Falha ao atualizar o status da venda.");
+                throw new FailCreateUpdateException("Falha ao atualizar o status da venda. Tente novamente!");
+            }
+
+            _logger.LogInformation("Status da venda atualizado com sucesso.");
+            return Ok("Status da venda atualizado com sucesso!");
         }
     }
 }
---
+++ AdaTech.WebAPI.SistemaVendas/Utilities/DTO/VendaDTO.cs
@@ -0,0 +1,21 @@
+﻿using AdaTech.WebAPI.DadosLibrary.DTO.Enums;
+using AdaTech.WebAPI.SistemaVendas.Utilities.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdaTech.WebAPI.SistemaVendas.Utilities.DTO
+{
+    public class VendaDTO
+    {
+        [Required(ErrorMessage = "É obrigatório informar a data da venda!")]
+        [DataType(DataType.DateTime)]
+        [NotFutureDate(ErrorMessage = "A data da venda não pode ser no futuro.")]
+        public DateTime DataVenda { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar o CPF do cliente!")]
+        [StringLength(11, ErrorMessage = "O CPF deve ter 11 dígitos!", MinimumLength = 11)]
+        public string ClienteCPF { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar o status da venda!")]
+        public StatusVenda StatusVenda { get; set; }
+    }
+}
